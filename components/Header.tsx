b1af406ab744da7d7abd14c2c1d289f24771ddf8
chenjianfeng <chenjianfeng9335@gmail.com>
2022-02-19 11:31:01 +0800

    feat: update link to github

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,6 +4,7 @@ import Twitter from './Icons/Twitter'
 import Breadcrumb from '../components/Breadcrumb/index'
 import { MoonIcon, SunIcon } from '@heroicons/react/solid'
 import { HeaderInterface } from '../interface/index'
+import Link from 'next/link'
 import NotesNav from '../components/Notes/Nav'
 const Header = ({ path='/ home', height=72, title='' }: HeaderInterface) => {
 
@@ -19,13 +20,15 @@ const Header = ({ path='/ home', height=72, title='' }: HeaderInterface) => {
     }
   }
 
+  const toGitHub = () => { window.location.href = 'https://github.com/saitoChen' }
+
   return (
     <div className="h-56">
       <div className={`w-100% h-${height} bg-block absolute top-0 -z-1 dark:bg-dbnewblock`}></div>
       <div className="flex flex-col font-mono text-base items-center mt-4 sm:flex-row sm:justify-between sm:px-4 lg:w-1024px lg:mx-auto">
         <div className="flex w-200px cursor-pointer fill-current justify-evenly text-highlight dark:text-grey sm:order-last">
           <div className="dark:hover:text-dbase"><Twitter /></div>
-          <div className="dark:hover:text-dbase"><GithubSvg /></div>
+          <div className="dark:hover:text-dbase" onClick={ toGitHub }><GithubSvg /></div>
           <div className="header-icon text-grey" onClick={ toggleDarkMode }>
             { darkModeOn ? <SunIcon/> : <MoonIcon /> }
           </div>
